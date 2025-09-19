d701be99b0e0a06a5ada569e1b59741de19a6fa6
Marlon <marlonyesid33@gmail.com>
2025-09-19 17:46:40 -0500

    Agregando atributos y metodos a la clase pila

--- Modelo/Pila.java
+++ Modelo/Pila.java
@@ -1,5 +1,37 @@
 package Modelo;
 
 public class Pila {
-    
+    Nodo cima;
+    int tamanio;
+    public Pila() {
+        this.cima = null;
+        this.tamanio = 0;
+    }
+
+    public boolean estaVacia() {
+        return cima == null;
+    }
+    public int getTamanio() {
+        return tamanio;
+    }
+    public void push(Pastor pastor) {
+        Nodo nuevoNodo = new Nodo(pastor);
+        if (estaVacia())
+            cima = nuevoNodo;
+        else {
+            nuevoNodo.setSiguiente(cima);
+            cima = nuevoNodo;
+        }
+        tamanio++;
+    }
+    public Pastor pop() {
+        if (estaVacia())
+            return null;
+        else {
+            Pastor pastor = cima.getPastor();
+            cima = cima.getSiguiente();
+            tamanio--;
+            return pastor;
+        }
+    }
 }
