61f489e1f10ffd1a55f48fa61726ff93808ff0cf
Marlon <marlonyesid33@gmail.com>
2025-09-21 21:54:55 -0500

    Creando el panel de opciones

--- Vista/PanelOpciones.java
+++ Vista/PanelOpciones.java
@@ -1,7 +1,18 @@
 package Vista;
 
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 public class PanelOpciones extends JPanel {
-    
+    private JButton izquierdaButton;
+    private JButton derechaButton;
+
+    public PanelOpciones() {
+        this.izquierdaButton = new JButton("Izquierda");
+        this.derechaButton = new JButton("Derecha");
+        this.add(izquierdaButton);
+        this.add(derechaButton);
+        this.setLayout(new FlowLayout());
+    }
 }
